11f856f4cce774c6e7047ff6b859c3fa3015c62d
down-one <80153075+down-one@users.noreply.github.com>
2021-03-08 18:09:22 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://shthey-my.sharepoint.com/:u:/g/personal/hrf_shthey_onmicrosoft_com/ESutbyX3u65Es33u2qU51E8BSCModrT18jsYkqo0nbTwnA?e=3qVnj1
+wget https://shthey-my.sharepoint.com/personal/hrf_shthey_onmicrosoft_com/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2Fhrf%5Fshthey%5Fonmicrosoft%5Fcom%2FDocuments%2F%E7%A7%8D%E5%AD%90%2Fs416%2Etorrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=1064; i=i+20, j=j+20))
 #do
