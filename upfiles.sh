22fea1ec807f85329500efa83824ca6af1ded7c1
down-one <80153075+down-one@users.noreply.github.com>
2021-03-08 18:06:50 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1219040.torrent
+wget https://shthey-my.sharepoint.com/:u:/g/personal/hrf_shthey_onmicrosoft_com/ESutbyX3u65Es33u2qU51E8BSCModrT18jsYkqo0nbTwnA?e=3qVnj1
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=1064; i=i+20, j=j+20))
 #do
