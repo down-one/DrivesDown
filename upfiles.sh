be6886ae132d7fdf379a507421e35508b4d5d9ce
down-one <80153075+down-one@users.noreply.github.com>
2021-03-11 20:38:50 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://share.acgnx.se/down.php?date=1604469760&hash=73086e86ca496da44a2a567b28334f5954d077c8
+wget https://bangumi.moe/download/torrent/5fa2440032f14c000721e90f/[VCB-Studio]%20Minami%20no%20Niji%20no%20Lucy%20_%20%E5%8D%97%E6%96%B9%E5%BD%A9%E8%99%B9%E9%9C%B2%E8%A5%BF%20_%20%E5%8D%97%E3%81%AE%E8%99%B9%E3%81%AE%E3%83%AB%E3%83%BC%E3%82%B7%E3%83%BC%2010-bit%20720p%20HEVC%20DVDRip%20[Fin].torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 for ((i=1, j=15; i<=650; i=i+15, j=j+15))
 do
