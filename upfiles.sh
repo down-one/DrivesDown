ecea8d1b915506f5c3de555cfb004a794353bb9a
down-one <80153075+down-one@users.noreply.github.com>
2021-03-12 21:12:20 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://nyaa.si/download/752635.torrent
+wget https://dl.dmhy.org/2015/06/20/faa6c00e08e6e1c387d08e9947fbafa5d1ed37f3.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 for ((i=1, j=15; i<=2500; i=i+15, j=j+15))
 do
