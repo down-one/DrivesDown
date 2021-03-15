e366cb12731ab8eb7e117616ffe6b20729858e9b
down-one <80153075+down-one@users.noreply.github.com>
2021-03-15 13:07:54 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://bangumi.moe/download/torrent/5de9e386306f1a0007c559e0/[VCB-Studio]%20RWBY%20_%20%E5%9B%9B%E8%89%B2%E6%88%98%E8%AE%B0%2010-bit%201080p%20HEVC%20BDRip%20[WEB%20Vol.1-Vol.4%20+%20SP%20Fin].torrent
+wget https://nyaa.si/download/1198747.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 for ((i=1, j=15; i<=2500; i=i+15, j=j+15))
 do
