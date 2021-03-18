427dcd92f5d193622cc0e64be770ba12e5308d44
down-one <80153075+down-one@users.noreply.github.com>
2021-03-19 00:25:20 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -2,7 +2,7 @@
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
 wget https://dl.dmhy.org/2014/12/08/2c20fac121281459b6ca35b2b6837f3f7c5f4acf.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=5; i<=2500; i=i+5, j=j+5))
+for ((i=1, j=2; i<=2500; i=i+2, j=j+2))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/待看/
