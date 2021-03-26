2fbbf34107dd50bbf22d44256c74e93fc60c5898
down-one <80153075+down-one@users.noreply.github.com>
2021-03-26 19:51:54 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://nyaa.si/download/1180334.torrent
+wget https://nyaa.si/download/1179558.torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=12; i<=2500; i=i+12, j=j+12))
 #do
@@ -8,4 +8,3 @@ aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_0 的文件"/番/
 #echo "$i-$j 上传完成"
 #done
-#总623 
