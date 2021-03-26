e0a4b60ff6d19c59901ea4d7103e3fbe3087c93a
down-one <80153075+down-one@users.noreply.github.com>
2021-03-26 19:50:25 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://nyaa.si/download/1182702.torrent
-#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=12; i<=2500; i=i+12, j=j+12))
-do
-aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
+wget https://nyaa.si/download/1180868.torrent
+aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
+#for ((i=1, j=12; i<=2500; i=i+12, j=j+12))
+#do
+#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_0 的文件"/番/
-echo "$i-$j 上传完成"
-done
+#echo "$i-$j 上传完成"
+#done
 #总623 
