b3f722dc18c9aa72e32815f374745e83babafed9
down-one <80153075+down-one@users.noreply.github.com>
2021-03-28 23:00:56 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://bangumi.moe/download/torrent/591700451a0b2308224480e1/[Nyamazing&VCB-Studio]%20LoveLive!%20Sunshine!!%20_%20%E3%83%A9%E3%83%96%E3%83%A9%E3%82%A4%E3%83%96%EF%BC%81%E3%82%B5%E3%83%B3%E3%82%B7%E3%83%A3%E3%82%A4%E3%83%B3!!%2010bit%201080p%20HEVC%20BDRip%20[Fin].torrent
+wget https://nyaa.si/download/1123040.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 for ((i=1, j=10; i<=900; i=i+10, j=j+10))
 do
