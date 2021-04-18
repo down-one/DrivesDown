da0cc1cc7c7194620d9926ecc6c39eb38b7d8c03
down-one <80153075+down-one@users.noreply.github.com>
2021-04-18 11:08:25 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -7,4 +7,4 @@ do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_1 的文件"/番/
 echo "$i-$j 上传完成"
-#done
+done
