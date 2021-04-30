9079d53c8853c58889c13b5f6e646093cc2b0f0b
down-one <80153075+down-one@users.noreply.github.com>
2021-04-30 19:24:13 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://nyaa.si/download/959117.torrent
+wget https://bangumi.moe/download/torrent/59b3b0edd04829c1623b1828/[VCB-Studio]%20Vivid%20Strike!%2010-bit%201080p%20HEVC%20BDRip%20[Fin].torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 for ((i=1, j=10; i<=700; i=i+10, j=j+10))
 do
