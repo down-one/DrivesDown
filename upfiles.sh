9057e759a8c2061198963234dbaddadb139d9c26
down-one <80153075+down-one@users.noreply.github.com>
2021-05-01 12:53:11 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://nyaa.si/download/949551.torrent
+wget https://bangumi.moe/download/torrent/59b3b0edd04829c1623b1828/[VCB-Studio]%20Vivid%20Strike!%2010-bit%201080p%20HEVC%20BDRip%20[Fin].torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=10; i<=700; i=i+10, j=j+10))
+for ((i=1, j=15; i<=700; i=i+15, j=j+15))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_1 的文件"/番/
