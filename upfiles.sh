a83fd0d09f03d9cf08fa797319cb393c29bfb553
down-one <80153075+down-one@users.noreply.github.com>
2021-05-16 17:02:00 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -2,7 +2,7 @@
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
 wget https://bangumi.moe/download/torrent/5908fdf46788ae4d5c97c63e/[VCB-Studio]%20Fune%20o%20Amu%20_%20%E7%BC%96%E8%88%9F%E8%AE%B0%2010-bit%201080p%20HEVC%20BDRip%20[Fin].torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=15; i<=2000; i=i+15, j=j+15))
+for ((i=46, j=55; i<=2000; i=i+10, j=j+10))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_1 的文件"/番/
