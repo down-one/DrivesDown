9adf2f85a27c22ba11c999f95628303bbf689684
down-one <80153075+down-one@users.noreply.github.com>
2021-09-18 21:16:01 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #aria2c "magnet:?xt=urn:btih:83908360bba6c64e39d89998193d40494dc8f9a0"
-wget https://sukebei.nyaa.si/download/3249324.torrent
-aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
+#wget https://sukebei.nyaa.si/download/3249324.torrent
+#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
+aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 "magnet:?xt=urn:btih:A8033851E65E4F7F88D628B39A636AB8E6E75E6D&dn=%5BFHD%5DSDMU-673"
 #for ((i=1, j=10; i<=2000; i=i+10, j=j+10))
 #do
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
