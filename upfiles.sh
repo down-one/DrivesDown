4319d63ee5a6aebb5897e2f14ff475bae5470292
down-one <hrf@shthey.onmicrosoft.com>
2022-07-26 06:30:13 +0800

    [Lilith-Raws] 來自深淵 劇場版總集篇 前編：啟程的黎明

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1556999.torrent
+wget https://nyaa.si/download/1556998.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 來自深淵 劇場版總集篇 後編：流浪之黃昏"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 來自深淵 劇場版總集篇 前編：啟程的黎明"
