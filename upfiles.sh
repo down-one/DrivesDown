f8b5dc1eadbbe624ef5d5f3cebba4e108ca4c029
down-one <hrf@shthey.onmicrosoft.com>
2022-08-02 06:30:14 +0800

    [Lilith-Raws] 轉生賢者的異世界生活～取得第二職業，成為世界最強～

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1560092.torrent
+wget https://nyaa.si/download/1560044.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 歡迎來到實力至上主義的教室"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 轉生賢者的異世界生活～取得第二職業，成為世界最強～"
