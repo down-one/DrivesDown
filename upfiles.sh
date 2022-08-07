6e87878a70e483a514bb0a468f1b2397a600b9b4
down-one <hrf@shthey.onmicrosoft.com>
2022-08-08 06:30:19 +0800

    [Lilith-Raws] 幽零幻鏡

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1563042.torrent
+wget https://nyaa.si/download/1562920.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 風都偵探"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 幽零幻鏡"
