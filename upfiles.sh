83313872ee799d50b2a0b735ac55b87392c89fbf
down-one <hrf@shthey.onmicrosoft.com>
2022-08-19 16:30:14 +0800

    [Lilith-Raws] 刀劍神域 Progressive 劇場版 無星夜的詠嘆調

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1567640.torrent
+wget https://nyaa.si/download/1567637.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] Fate/Grand Order - 終局特異點 冠位時間神殿所羅門 / Fat"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 刀劍神域 Progressive 劇場版 無星夜的詠嘆調"
