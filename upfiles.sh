b16edf947afabc9407e9aacbeca2cf27dc69c303
down-one <hrf@shthey.onmicrosoft.com>
2022-09-26 06:30:25 +0800

    [Lilith-Raws] 機動戰士鋼彈 水星的魔女

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1582974.torrent
+wget https://nyaa.si/download/1582914.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 聯盟空軍航空魔法音樂隊 光輝魔女"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 機動戰士鋼彈 水星的魔女"
